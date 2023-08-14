4d569888cbf797d5592932f0a7d51c42644911ea
Ramonglez1 <141690356+Ramonglez1@users.noreply.github.com>
2023-08-14 14:22:23 -0600

    Update sarcopenia.py

--- sarcopenia.py
+++ sarcopenia.py
@@ -1,8 +1,6 @@
 
 st.write('Las mediciones se realizan con un dinamómetro hidráulico manual, el cual se utiliza para medir la fuerza de agarre. Es utilizado como herramienta diagnóstica y evolución de un paciente, en este caso como criterio para el diagnóstico de sarcopenia') 
 
-git clone https://github.com/Ramonglez1/Practica/edit/main/sarcopenia
-
 st.write("ello")
 
 
