4d9fa9a3ab60f008196a87e544d67b2c601e2070
Ramonglez1 <141690356+Ramonglez1@users.noreply.github.com>
2023-08-15 09:37:16 -0600

    Update tododesorcopenia.py

--- tododesorcopenia.py
+++ tododesorcopenia.py
@@ -1,1 +1,8 @@
+st.title("sarcopenia") 
+
+st.write("Calculo de Fuerza de agarre
+Las mediciones se realizan con un dinamómetro hidráulico manual, el cual se utiliza para medir la fuerza de agarre. Es utilizado como herramienta diagnóstica y evolución de un paciente, en este caso como criterio para el diagnóstico de sarcopenia.
+
+La medición de la fuerza de agarre máxima (MGS) es un elemento esencial para evaluar a la fuerza durante el crecimiento, envejecimiento, en las secuelas de lesiones traumáticas y en la rehabilitación. El dinamómetro es considerado un instrumento adecuado y confiable para la evaluación de la fuerza de prensión manual del paciente, aunque la fiabilidad de la evaluación puede verse afectada por género, peso y la postura corporal") 
+
 
