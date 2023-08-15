d8d156ac6fbee22856a78707a7d1a252e6b0923d
Ramonglez1 <141690356+Ramonglez1@users.noreply.github.com>
2023-08-15 13:36:08 -0600

    Update 1_text.py

--- pages/1_text.py
+++ pages/1_text.py
@@ -1,1 +1,19 @@
+import streamlit as st
+from PIL import Image
+import pandas as pd #importa la paquetería PAnel DAta (llamada pandas)
+#pip install matplotlib_venn
+from matplotlib_venn import venn2, venn2_circles, venn2_unweighted # importa paqueteria para graficar diagramas de venn
+from matplotlib_venn import venn3, venn3_circles
+from matplotlib import pyplot as plt #importa pyplot para hacer gáficas
+from matplotlib import numpy as np #importar numpy
+import seaborn as sn
+import altair as alt
+#pip install altair_catplot
+import altair_catplot as altcat
+#pip install xlsxwriter
+import xlsxwriter
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn as sk
 st.write("algo")
